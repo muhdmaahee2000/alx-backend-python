acaa6065a4fea6f7199a9d61aa0fc487d5cd5ada
muhdmaahee2000 <youngzmahi99@gmail.com>
2023-07-15 01:18:20 +0100

    creates tables, ranks country origins of bands,lists all bands with Glam rock as their main style,creates a trigger that decreases.

--- 0x02-python_async_comprehension/0x00-MySQL_Advanced/3-glam_rock.sql
+++
@@ -1,8 +0,0 @@
--- Ranks longevity of Glam bands
--- Column names must be: band_name and lifespan (in years)
-
-SELECT
-band_name, ifnull(split, 2020)-ifnull(formed, 0) AS lifespan
-FROM metal_bands
-WHERE style LIKE "%Glam rock%"
-ORDER BY lifespan DESC;
--- 0x02-python_async_comprehension/0x00-MySQL_Advanced/4-store.sql
+++
@@ -1,9 +0,0 @@
--- Creates trigger to decrease quantity when order added
-
-CREATE TRIGGER 
-reduce_quantity
-AFTER INSERT ON orders 
-FOR EACH ROW 
-UPDATE items
-SET quantity = quantity - NEW.number
-WHERE name = NEW.item_name;-- Creates trigger to decrease quantity when order added
